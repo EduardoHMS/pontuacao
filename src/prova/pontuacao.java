b73f2d9bd0ed704eb3d2a47129ccfd0b08df9262
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2020-05-15 19:14:32 -0300

    Cabecalho

--- src/prova/pontuacao.java
+++ src/prova/pontuacao.java
@@ -1,10 +1,20 @@
 package prova;
 
+import java.util.Scanner;
+
 public class pontuacao {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		// CABECALHO //
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println(" ");
+		
+		
+		sc.close();
 	}
 
 }
